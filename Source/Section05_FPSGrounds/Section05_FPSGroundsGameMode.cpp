4b4d618824db2bee20adf38e30a3838b3289c12f
Joel Lee <lee.joel395@gmail.com>
2019-02-10 16:37:58 -0800

    Refactored files. Moslty consolidating FirstPerson and NPC to Character.

--- Source/Section05_FPSGrounds/Section05_FPSGroundsGameMode.cpp
+++ Source/Section05_FPSGrounds/Section05_FPSGroundsGameMode.cpp
@@ -9,7 +9,7 @@ ASection05_FPSGroundsGameMode::ASection05_FPSGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Blueprint/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
